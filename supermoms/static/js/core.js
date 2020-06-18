7246e5518a8ef2c6f3addc74fd5baaa25e839e0c
Alexander Liao <alex.yj.liao@gmail.com>
2020-06-18 05:43:48 -0400

    design core of index and signup

---
+++ supermoms/static/js/core.js
@@ -0,0 +1,23 @@
+function setlang(lang, no_reload) {
+  var clang = getlang();
+  var now = new Date();
+  now.setTime(now.getTime() + 365 * 24 * 60 * 60 * 1000);
+  document.cookie = "lang=" + lang + ";expires=" + now.toGMTString() + ";path=/";
+  if (!no_reload && clang != lang) location.reload();
+}
+
+function getlang() {
+  var name = "lang=";
+  var decoded = decodeURIComponent(document.cookie);
+  var ca = decoded.split(";");
+  for (var c of ca) {
+    while (c.charAt(0) == " ") c = c.substring(1);
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length);
+    }
+  }
+}
+
+window.onload = function() {
+  setlang(getlang() || "EN", true);
+}
